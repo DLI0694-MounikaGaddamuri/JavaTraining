401295df50b21b229d6b27c08bf071eae4d3dfd3
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-25 22:19:42 +0530

    Create AdditionSubtraction.java

---
+++ Assignment2/Operators Programs/Arithmetic Operators/AdditionSubtraction.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AdditionSubtraction{
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any two numbers: ");
+        int n1=input.nextInt();
+        int n2=input.nextInt();
+        System.out.println("Addition: "+(n1+n2));
+        System.out.println("Subtraction: "+(n1-n2));
+}
