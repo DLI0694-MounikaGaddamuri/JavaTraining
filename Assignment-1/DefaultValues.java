789f012917568694d008ab9c4a364b9f09c17091
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-25 20:19:26 +0530

    Create DefaultValues.java

---
+++ Assignment-1/DefaultValues.java
@@ -0,0 +1,19 @@
+public class DefaultValues{
+    static int intNumber;
+    static float floatNumber;
+    static char charVar;
+    static double doubleNumber;
+    static byte byteNumber;
+    static short shortNumber;
+    static boolean bool;
+    public static void main(String[] args){
+        System.out.println("Default values of Primitive data types:");
+        System.out.println("int: "+intNumber);
+        System.out.println("float: "+floatNumber);
+        System.out.println("char: "+charVar);
+        System.out.println("double: "+doubleNumber);
+        System.out.println("byte: "+byteNumber);
+        System.out.println("short: "+shortNumber);
+        System.out.println("boolean: "+bool);
+  }
+}
