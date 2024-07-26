641f8529055944cd3433d3d9c69e0ecf6b660f3e
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-26 09:52:52 +0530

    Create ToggleBoolean.java

---
+++ Assignment2/Operators Programs/Unary Operators/ToggleBoolean.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ToggleBoolean{
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean b=true;
+        System.out.println(!b);
+  }
+}
