e51955ce854d57a66b973d493661d4f3c95ca4a2
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-26 09:49:06 +0530

    Create Compound.java

---
+++ Assignment2/Operators Programs/Assignment Operators/Compound.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Compound{
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int dup=12;
+        dup+=2;
+        System.out.println("+= : "+dup);
+        dup-=2;
+        System.out.println("-= : "+dup);
+        dup*=2;
+        System.out.println("*= : "+dup);
+        dup/=2;
+        System.out.println("/= : "+dup);
+        dup%=2;
+        System.out.println("%= : "+dup);
+  }
+}
