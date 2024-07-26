dedf19260675e129b9d3c92d1e9cf4358c3f19af
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-26 09:51:26 +0530

    Create PostPreIncrement.java

---
+++ Assignment2/Operators Programs/Unary Operators/PostPreIncrement.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class PostPreIncrement{
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int postAndPre=4;
+        System.out.println("Post Increment: "+postAndPre++);
+        System.out.println("Pre Increment: "+ ++postAndPre);
+  }
+}
