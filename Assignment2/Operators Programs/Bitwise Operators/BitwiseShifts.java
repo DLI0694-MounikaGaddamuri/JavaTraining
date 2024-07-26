ff537f2246da40d49def90949946aa4508528f65
Mounika-Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-07-26 09:57:18 +0530

    Create BitwiseShifts.java

---
+++ Assignment2/Operators Programs/Bitwise Operators/BitwiseShifts.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class BitwiseShifts{
+  public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int bit3=input.nextInt();
+        System.out.println("Left Shift: "+ (bit3<<2));  //multiplication
+        System.out.println("Right Shift: "+ (bit3>>2)); //division
+  }
+}
